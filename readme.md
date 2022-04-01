cdd0dec780adc1796b0af9d96fb84c9993c12af0
briandenicola <brian@briandenicola.com>
2022-03-31 22:29:37 -0500

    Rewrite for minimal api

--- readme.md
+++ readme.md
@@ -2,10 +2,21 @@
 Sometimes you just need a very simple API to test connectivity. This is my super simple dotnet version.
 
 # Build
-1. scripts/publish.bat
-
+```bash
+scripts/publish.bat
+```
 # Run
-1. \src\publish\windows\api.exe
+```bash
+\src\publish\windows\api.exe
+```
 
 # Test
-1. curl http://(any_ip_assigned_to_machine):5500/weatherforecast
+```bash
+curl -v http://localhost:5500/
+curl -v -X POST http://localhost:5500/todos/ -d '{"Id": 1, "Name": "Take out trash"}' -H "Content-Type: application/json"
+curl -v -X POST http://localhost:5500/todos/ -d '{"Id": 2, "Name": "Clean your bathroom"}' -H "Content-Type: application/json"
+curl -v http://localhost:5500/todos/1
+curl -v -X PUT http://localhost:5500/todos/1 -d '{"Name": "Take out trash", "IsComplete": true }' -H "Content-Type: application/json"
+curl -v http://localhost:5500/todos/1
+curl -v http://localhost:5500/todos/
+```
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,53 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-builder.Services.AddControllers();
-builder.WebHost.UseKestrel(opts =>
-{
-    opts.ListenAnyIP(5500);
-});
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
-app.MapControllers();
+app.Urls.Add("http://+:5500");
+
+app.MapGet( "/", () =>  $"Hello World! The time now is {DateTime.Now}" );
+
+app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
+
+app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound()
+);
+
+app.MapPost("/todos", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todos/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
 app.Run();
+
+class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)  : base(options) { }
+    public DbSet<Todo> Todos => Set<Todo>();
+}
