82084880baf683b5bdda4151b3d21ec368bbb8b3
Brian <brian@briandenicola.com>
2022-06-20 20:16:34 +0000

    k3d clster

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,10 +5,8 @@
 echo "$(date)    post-create start" >> ~/status
 
 #Install k3s.ios
-#curl -sfL https://get.k3s.io | sh -
-#sudo k3s server &
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-k3d cluster create mycluster
+k3d cluster create myapp
 
 # create local registry
 docker network create k3d
@@ -18,6 +16,6 @@ docker network connect k3d k3d-registry.localhost
 # update the base docker images
 docker pull mcr.microsoft.com/dotnet/sdk:6.0-alpine
 docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull bjd145/utils:3.9
 
 echo "$(date)    post-create complete" >> ~/status
