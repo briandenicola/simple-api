3261c1aeed8d583e7ba8aaeb2f6250eac9b4fcbe
Brian <brian@briandenicola.com>
2022-06-20 19:35:57 +0000

    Moved to post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,4 +4,17 @@
 
 echo "$(date)    post-create start" >> ~/status
 
+#Install k3s.ios
+curl -sfL https://get.k3s.io | sh -
+
+# create local registry
+docker network create k3d
+k3d registry create registry.localhost --port 5500
+docker network connect k3d k3d-registry.localhost
+
+# update the base docker images
+docker pull mcr.microsoft.com/dotnet/sdk:6.0-alpine
+docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+
 echo "$(date)    post-create complete" >> ~/status
