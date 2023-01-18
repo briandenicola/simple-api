10bf077af92b183fe4c39cc0902f6c5a7d047d87
Brian <brian@briandenicola.com>
2023-01-18 16:15:27 +0000

    Updated to dotnet7

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -18,8 +18,8 @@ k3d registry create registry.localhost --port 5500
 docker network connect k3d k3d-registry.localhost
 
 # update the base docker images
-docker pull mcr.microsoft.com/dotnet/sdk:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:7.0-alpine
+docker pull mcr.microsoft.com/dotnet/aspnet:7.0-alpine
 docker pull bjd145/utils:3.9
 
 echo "$(date)    post-create complete" >> ~/status
--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,6 +1,4 @@
 @echo off
 
 cd ..\src
-dotnet publish -c Release -r osx.11.0-arm64 --self-contained --nologo -o publish/mac -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish -c Release -r linux-x64 --self-contained --nologo -o publish/linux -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
 dotnet publish -c Release -r win10-x64 --self-contained --nologo -o publish/windows -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
