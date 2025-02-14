9860def207e5ebb7ec8de2d3f8bd64c5bf10ffa5
briandenicola <brian@briandenicola.com>
2025-02-14 08:59:43 -0600

    🏐 Fri Feb 14 08:59:43 CST 2025

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,18 +8,10 @@ echo "$(date)    post-create start" >> ~/status
 sudo apt update
 sudo apt install -y jq
 
-#Install k3s.ios
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-k3d cluster create myapp
+#Install Task
+sudo sh -c "$(curl -sL https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
 
-# create local registry
-docker network create k3d
-k3d registry create registry.localhost --port 5500
-docker network connect k3d k3d-registry.localhost
-
-# update the base docker images
-docker pull mcr.microsoft.com/dotnet/sdk:7.0-alpine
-docker pull mcr.microsoft.com/dotnet/aspnet:7.0-alpine
-docker pull bjd145/utils:3.9
+#Helpful Utils container
+docker pull bjd145/utils:3.20
 
 echo "$(date)    post-create complete" >> ~/status
